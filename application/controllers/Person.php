22da661cd4bc186b10a21c3db40a02ca7dd3209d
Putya Sekar Ayu <5198258-putyasekar@users.noreply.gitlab.com>
2020-12-10 10:20:41 +0700

    hapus catatan

--- application/controllers/Person.php
+++ application/controllers/Person.php
@@ -4,19 +4,17 @@ require APPPATH . 'libraries/REST_Controller.php';
 
 class Person extends REST_Controller{
 
-  // construct
+
   public function __construct(){
     parent::__construct();
     $this->load->model('PersonM');
   }
 
-  // method index untuk menampilkan semua data product menggunakan method get
   public function index_get(){
     $response = $this->PersonM->all_product();
     $this->response($response);
   }
 
-  // untuk menambah product menaggunakan method post
   public function add_post(){
     $response = $this->PersonM->add_product(
         $this->post('nama'),
@@ -28,13 +26,11 @@ class Person extends REST_Controller{
     $this->response($response);
   }
 
-  // update data product menggunakan method put
   public function update_put(){
     $response = $this->PersonM->update_product(
         $this->put('id'),
         $this->put('nama'),
         $this->put('kelas'),
-        // $this->put('image'),
         $this->put('pelajaran'),
         $this->put('keterangan_masuk'),
         $this->put('tanggal')
@@ -42,7 +38,6 @@ class Person extends REST_Controller{
     $this->response($response);
   }
 
-  // hapus data product menggunakan method delete
   public function delete_delete(){
     $response = $this->PersonM->delete_product(
         $this->delete('id')
--- application/models/PersonM.php
+++ application/models/PersonM.php
@@ -1,9 +1,7 @@
 <?php
 
-// extends class Model
 class PersonM extends CI_Model{
 
-  // response jika field ada yang kosong
   public function empty_response(){
     $response['status']=502;
     $response['error']=true;
@@ -11,7 +9,6 @@ class PersonM extends CI_Model{
     return $response;
   }
 
-  // function untuk insert data ke tabel tb_product
   public function add_product($nama,$kelas,$pelajaran,$keterangan_masuk,$tanggal){
 
     if(empty($nama) || empty($kelas) || empty($pelajaran) || empty($keterangan_masuk) || empty($tanggal)){
@@ -42,18 +39,14 @@ class PersonM extends CI_Model{
 
   }
 
-  // mengambil semua data product
   public function all_product(){
 
     $all = $this->db->get("tb_absen")->result();
-    // $response['status']=200;
-    // $response['error']=false;
     $response = $all;
     return $response;
 
   }
 
-  // hapus data product
   public function delete_product($id){
 
     if($id == ''){
@@ -80,7 +73,6 @@ class PersonM extends CI_Model{
 
   }
 
-  // update product
   public function update_product($id,$nama,$kelas,$pelajaran,$keterangan_masuk,$tanggal){
 
     if($id == '' || empty($nama) || empty($kelas) || empty($pelajaran) || empty($keterangan_masuk) || empty($tanggal)){
@@ -101,11 +93,9 @@ class PersonM extends CI_Model{
       $this->db->where($where);
       $update = $this->db->update("tb_absen",$set);
       if($update){
-        // $response['status']=200;
-        // $response['error']=false;
-        // $response['message']='Data product diubah.';
         $response=$where+$set;
         return $response;
+
       }else{
         $response['status']=502;
         $response['error']=true;
